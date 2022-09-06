3bc78d0d1b0536dd3dc72c8d3620e6b54e5c289c
MateusDuraessantos <mateusduraessantos@datamachina.com.br>
2022-09-05 21:48:40 -0300

    Page cursos

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <el-button>
-      mateus
-    </el-button>
-
     <NewHeader />
     <router-view />
     <NewFooter />
@@ -23,10 +19,13 @@ export default {
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+/* font-family: 'Roboto', sans-serif; */
 
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;500;600;700;800;900&display=swap");
+/* font-family: 'Roboto Slab', serif; */
 * {
-  font-family: "roboto";
+  font-family: "Roboto", sans-serif;
   padding: 0;
   margin: 0;
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import homeMack from '../views/homeMack'
 import MackenzieSolucoes from '../views/MackenzieSolucoes.vue'
+import CursoMack from '../views/CursoMack.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/MackenzieSolucoes',
     name: 'MackenzieSolucoes',
     component: MackenzieSolucoes
+  },
+  {
+    path: '/CursoMack',
+    name: 'CursoMack',
+    component: CursoMack
   },
 
 ]
